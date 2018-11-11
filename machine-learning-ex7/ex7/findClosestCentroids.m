647609d768cca1fcea12bb378a4164f0b9a421b1
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2018-11-11 18:55:06 +0300

    7-th week finished

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -7,23 +7,23 @@ function idx = findClosestCentroids(X, centroids)
   m = size(X, 1);
   %% my mid. perp. enhancement
   % vector of middle perpendicular hyperplane of 2 covectors
-  midPerp = @(a, b) [b - a, - (b - a) * mean([a; b])']';
-  midPerps = cell(K, K);
-  for i = 1:K
-    for j = i:K
-      midPerps{i,j} = midPerp(centroids(i,:), centroids(j,:));
-      midPerps{j,i} = -midPerps{i,j};
-    end
-  end
-  idx = zeros(m, 1);
-  for p = 1:m
-    closeness = cellfun(@(perp) [X(p,:) 1] * perp >= 0, midPerps);
-    idx(p) = find(all(closeness, 1));
-  end
+  %midPerp = @(a, b) [b - a, - (b - a) * mean([a; b])']';
+  %midPerps = cell(K, K);
+  %for i = 1:K
+  %  for j = i:K
+  %    midPerps{i,j} = midPerp(centroids(i,:), centroids(j,:));
+  %    midPerps{j,i} = -midPerps{i,j};
+  %  end
+  %end
+  %idx = zeros(m, 1);
+  %for p = 1:m
+  %  closeness = cellfun(@(perp) [X(p,:) 1] * perp >= 0, midPerps);
+  %  idx(p) = find(all(closeness, 1));
+  %end
   %% and it does work!
   %% but sometimes strange conformance bug?!
-  % idx = zeros(m, 1);
-  % for i = 1:m
-  %   [_ idx(i)] = min(sumsq(X(i,:) - centroids, 2));
-  % end
+  idx = zeros(m, 1);
+  for i = 1:m
+    [_ idx(i)] = min(sumsq(X(i,:) - centroids, 2));
+  end
 end
--- machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -4,6 +4,6 @@ function centroids = kMeansInitCentroids(X, K)
 % centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
 % used with the K-Means on the dataset X
   randidx = randperm(size(X, 1));
-  centroids = X(randidx(1:K), :):
+  centroids = X(randidx(1:K), :);
 end
 
